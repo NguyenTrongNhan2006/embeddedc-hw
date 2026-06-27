ea872832e22cbcc4aec71aa2a1a4b73dddf6496d
Nguyen Trong Nhan <Nguyenvanmuoi0882@gmail.com>
2026-06-27 19:55:39 +0700

    Update main.c

--- week01/main.c
+++ week01/main.c
@@ -1,15 +1,25 @@
 // HW01: HỆ THỐNG GIÁM SÁT LÒ NHIỆT CÔNG NGHIỆP
 
-#include <stdio.h>
-#include <stdint.h>
+#include <stdio.h>  // standard input/output
+#include <stdint.h> // fixed-width integer types
 
-// TASK 1: POINTERS & MEMORY
+// TASK 1: POINTERS & MEMORY : ( Bóc tách gói tin cấu hình )
+
+// const uint8_t *config_packet: con trỏ hằng trỏ đến gói tin cấu hình , dữ liệu đầu vào, gồm 2 byte
+// int16_t *high_threshold: con trỏ đến biến lưu giá trị ngưỡng , nơi lưu kết quả sau khi ghép 2 byte thành số nhiệt độ
 
 void parse_config(const uint8_t *config_packet, int16_t *high_threshold)
 {
     // HỌC VIÊN BẮT ĐẦU VIẾT CODE TỪ ĐÂY
 
-    // abcd
+    // Kiểm tra NULL pointer để tránh crash
+    if (config_packet == 0 || high_threshold == 0)
+    {
+        return;
+    }
+    // Ghép 2 byte từ config_packet LSB & MSB thành một số nguyên 16-bit và lưu vào high_threshold
+    *high_threshold = (int16_t)(((uint16_t)config_packet[0]) |
+                                ((uint16_t)config_packet[1] << 8));
 
     // HỌC VIÊN KẾT THÚC VIẾT CODE
 }
